dae0fb5ac628edfffc25079ebf7b2ef169c1481d
ahmed-nabil2 <ahmed.nabil@vultara.com>
2024-09-22 19:01:33 +0300

    set up mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-
-
 const app = require('./app');
+const connectDB = require('./db/dbConnection');
+
 const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);

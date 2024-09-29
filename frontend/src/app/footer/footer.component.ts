c763250dbf429dab46c0aca52ea4cec8fd3f6fdb
ahmed-nabil2 <ahmed.nabil@vultara.com>
2024-09-29 17:51:40 +0300

    implement basic footer

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, 
+    RouterLink, 
+    MatButtonModule, 
+    MatIconModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })

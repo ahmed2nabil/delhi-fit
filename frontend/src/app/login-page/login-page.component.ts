65c87089c52fbf5d0d3346faaaec62d2e24aa204
ahmed2nabil <ahmednabl992@gmail.com>
2024-10-06 19:46:50 +0300

    add basic styling for login page

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule, 
+    CommonModule, 
+    MatButtonModule, 
+    MatInputModule, 
+    MatFormFieldModule,
+     MatCardModule, 
+    RouterModule,
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  signInForm: FormGroup;
+  error$!: Observable<any>;
+  constructor(private fb: FormBuilder) {
+      this.signInForm = this.fb.group({
+          username: ['', Validators.required],
+          password: ['', Validators.required]
+      });
+  }
+  onSubmit() {
 
+  }
 }
